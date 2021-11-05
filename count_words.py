12def820ab2d0ac50c2d28a682a89d6d04b14659
Wei Xuan <xuan-wei@hotmail.com>
2021-11-05 22:02:56 +0100

    Make prediction

--- count_words.py
+++ count_words.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from nltk.tokenize import word_tokenize
 
+
 class MLAnalyzeData:
     def __init__(self):
         self.classifier = None
@@ -62,7 +63,7 @@ class MLAnalyzeData:
         y = dataset.iloc[:, 3].values
         return x, y
 
-    def logistic_regression(x, y):
+    def logistic_regression(self, x, y):
         # Split the dataset into training dataset and testing dataset
         from sklearn.model_selection import train_test_split
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=None)
@@ -110,8 +111,61 @@ class MLAnalyzeData:
         y_predict = self.analyze_data(x_test)
         self.print_accuracy(y_test, y_predict)
 
+    def prepare_twitt_matrix(self, dict):
+        with open('disaster_words.txt') as file:
+            raw_content = file.read()
+            contents_1 = raw_content.lower().split()
+
+        with open('relevant_words.txt') as file:
+            raw_content = file.read()
+            contents_2 = raw_content.lower().split()
+
+        filename = 'mongodb.csv'
+        with open(filename, 'w') as file_object:
+            file_object.write(f"tweet number, number of disaster words, number of relevant words\n")
+
+        n = 0
+        for key, value in dict.items():
+            n = n + 1
+            tokens = word_tokenize(value)
+            # print(f"{n}. {tokens}")
+
+            i = 0
+            j = 0
+            for token in tokens:
+                token = token.lower()
+                if token in contents_1:
+                    i = i + 1
+                    # print(f"The word \"{token}\" is a disaster word.")
+                elif token in contents_2:
+                    j = j + 1
+                    # print(f"The word \"{token}\" is a relevant word.")
+                else:
+                    continue
+                    # print(f"The word \"{token}\" is neither a disaster word nor a relevant word.")
+
+            # print(f"number of disaster words: {i}")
+            # print(f"number of relevant words: {j}\n")
+
+            with open(filename, 'a') as file_output:
+                file_output.write(f"{n}, {i}, {j}\n")
+
+    def load_mongo_data_matrix(self):
+        # Import dataset
+        dataset = pd.read_csv('mongodb.csv')
+        x = dataset.iloc[:, [1, 2]].values
+        return x
+
+    def give_lable(self, matrix):
+        y = self.analyze_data(matrix)
+        print(y)
+
+    def analyze_mongo_data(self, dict):
+        self.prepare_twitt_matrix(dict)
+        matrix = self.load_mongo_data_matrix()
+        self.give_lable(matrix)
 
 if __name__ == '__main__':
     # Test complete run for test data
-    #read_training_data_file(file_name='train.csv')
+    # read_training_data_file(file_name='train.csv')
     mla = MLAnalyzeData()
--- invoke_prediction.py
+++ invoke_prediction.py
@@ -1,4 +1,19 @@
 import count_words
 
-mla = count_words.MLAnalyzeData()
 #mla.analyze_data(x_test=?)
+
+dictionary = {1:"today is thursday",
+              2: "tomorrow is friday",
+              3: "when will it rain",
+              4: "when can i see sunshine",
+              5: "hurricane"}
+
+
+if __name__ == '__main__':
+    # One line invocation
+    mla = count_words.MLAnalyzeData().analyze_mongo_data(dictionary)
+
+    # Also possible to invoke the model once
+    # mla = count_words.MLAnalyzeData()
+
+    # And then call mla.analyze_mongo_data(dictionary) for every dictionary
